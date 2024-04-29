f373889ae5045b6291d6bc30be766f7b519ef9b8
Carlos Faustino <chfcchfc96@gmail.com>
2024-04-29 14:25:24 -0300

    feat: config cypress

---
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -0,0 +1,35 @@
+describe("add product to cart", () => {
+  beforeEach(() => {
+    cy.visit("/")
+  })
+
+  it("should be able to navigate to the product page and add it to the cart", () => {
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.url().should("include", "/product")
+    cy.contains("Adicionar ao carrinho").click()
+
+    cy.contains("Cart (1)").should("exist")
+  })
+
+  it("should not count duplicated products on cart", () => {
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.url().should("include", "/product")
+    cy.contains("Adicionar ao carrinho").click()
+    cy.contains("Adicionar ao carrinho").click()
+
+    cy.contains("Cart (1)").should("exist")
+  })
+
+  it("should be able to seeach for a product and add it to the cart", () => {
+    cy.searchByQuery("moletom")
+
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.url().should("include", "/product")
+    cy.contains("Adicionar ao carrinho").click()
+
+    cy.contains("Cart (1)").should("exist")
+  })
+})
