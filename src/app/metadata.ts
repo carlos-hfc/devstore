4ec08c4e6ef394d0a736d24731fa30181e7c470b
Carlos Faustino <chfcchfc96@gmail.com>
2024-04-29 12:45:42 -0300

    feat: update metadata

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -8,32 +8,12 @@ export const config: Metadata = {
   description: "Produtos voltados para devs.",
   twitter: {
     card: "summary_large_image",
-    images: {
-      url: "/favicon/favicon2048x2048.png",
-      alt: "devstore",
-    },
   },
   openGraph: {
     type: "website",
     url: new URL(String(process.env.NEXT_PUBLIC_BASE_URL)),
     locale: "pt_BR",
     siteName: "devstore",
-    images: {
-      url: "/favicon/favicon2048x2048.png",
-      alt: "devstore",
-    },
-  },
-  icons: {
-    icon: [
-      { url: "/icon.png", type: "image/png", sizes: "180x180" },
-      { url: "/favicon.ico", type: "image/x-icon", sizes: "48x48" },
-    ],
-    apple: { url: "/apple-icon.png", type: "image/png", sizes: "180x180" },
-    shortcut: "/favicon/shortcut-icon.png",
-    other: {
-      rel: "mask-icon",
-      url: "/favicon/safari-pinned-tab.svg",
-    },
   },
   metadataBase: new URL(String(process.env.NEXT_PUBLIC_BASE_URL)),
   alternates: {
